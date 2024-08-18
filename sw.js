a40c21b1e3c9370de813964de1a728b42b0844fd
GITA-HIRO <nijika@192.168.1.80>
2024-08-18 21:13:23 +0800

    Site updated: 2024-08-18 21:13:23

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1723984875283";
+const VERSION = "1723986802377";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
